dc04b94b2b2d6d1c9c68bd310cd005f0188337be
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-08-11 15:06:42 +0800

    Update bonezegei-mobile.js

--- src/bonezegei-mobile.js
+++ src/bonezegei-mobile.js
@@ -1,1 +1,52 @@
+/*Bonezegei Mobile Template
+  Author: Jofel Batutay (Bonezegei)
+  Date: August 2023 
+*/
+
+var colorNavBarButtonActive = "#000";
+var colorNavBarButton = "#888";
+
+navbarButton4_1.onclick = function () {
+  navbarButton4_1.style.color = colorNavBarButtonActive;
+  navbarButton4_2.style.color = colorNavBarButton;
+  navbarButton4_3.style.color = colorNavBarButton;
+  navbarButton4_4.style.color = colorNavBarButton;
+  navbarContent4_1.style.display = "block";
+  navbarContent4_2.style.display = "none";
+  navbarContent4_3.style.display = "none";
+  navbarContent4_4.style.display = "none";
+};
+
+navbarButton4_2.onclick = function () {
+  navbarButton4_1.style.color = colorNavBarButton;
+  navbarButton4_2.style.color = colorNavBarButtonActive;
+  navbarButton4_3.style.color = colorNavBarButton;
+  navbarButton4_4.style.color = colorNavBarButton;
+  navbarContent4_1.style.display = "none";
+  navbarContent4_2.style.display = "block";
+  navbarContent4_3.style.display = "none";
+  navbarContent4_4.style.display = "none";
+};
+
+navbarButton4_3.onclick = function () {
+  navbarButton4_1.style.color = colorNavBarButton;
+  navbarButton4_2.style.color = colorNavBarButton;
+  navbarButton4_3.style.color = colorNavBarButtonActive;
+  navbarButton4_4.style.color = colorNavBarButton;
+  navbarContent4_1.style.display = "none";
+  navbarContent4_2.style.display = "none";
+  navbarContent4_3.style.display = "block";
+  navbarContent4_4.style.display = "none";
+};
+
+navbarButton4_4.onclick = function () {
+  navbarButton4_1.style.color = colorNavBarButton;
+  navbarButton4_2.style.color = colorNavBarButton;
+  navbarButton4_3.style.color = colorNavBarButton;
+  navbarButton4_4.style.color = colorNavBarButtonActive;
+  navbarContent4_1.style.display = "none";
+  navbarContent4_2.style.display = "none";
+  navbarContent4_3.style.display = "none";
+  navbarContent4_4.style.display = "block";
+};
 
